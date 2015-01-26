94a3c17b74b1f747aa4820461404f2a2d39527ce
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-01-26 17:15:00 -0600

    assignment 3 added

---
+++ homework/assignment3/scripts/assign3.js
@@ -0,0 +1,37 @@
+$(document).ready(onReady);
+
+function onReady() {
+	$('#searchbutton').on('click', onSearchButtonClick);
+
+	
+function onSearchButtonClick() {
+		imdbSearch($('#search-box').val());
+		
+	}
+
+
+
+function imdbSearch(query)	{
+		$.get(
+			'http://www.omdbapi.com',
+			{s: query,
+				},
+			onSearchResults, 
+			'json'
+			);
+	}
+
+
+function onSearchResults(data){
+		
+		
+
+
+var i = 0;
+
+for(var key in data.Search)	{ 
+	$('#results').append('<tr> <td>'+data.Search[i].Title+'</td> <td>'+data.Search[i].Year+'</td><td>'+data.Search[i].Type+'</td></tr>');
+	i++;
+}
+}
+}
