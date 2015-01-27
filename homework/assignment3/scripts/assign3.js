2d09144c6cf2deafaabc6a617f10b6872afcfbe9
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-01-26 18:03:34 -0600

    updates

--- homework/assignment3/scripts/assign3.js
+++ homework/assignment3/scripts/assign3.js
@@ -30,7 +30,7 @@ function onSearchResults(data){
 var i = 0;
 
 for(var key in data.Search)	{ 
-	$('#results').append('<tr> <td>'+data.Search[i].Title+'</td> <td>'+data.Search[i].Year+'</td><td>'+data.Search[i].Type+'</td></tr>');
+	$('#results').append('<tr><td>'+data.Search[i].Title+'</td> <td>'+data.Search[i].Year+'</td><td>'+data.Search[i].Type+'</td></tr>');
 	i++;
 }
 }
